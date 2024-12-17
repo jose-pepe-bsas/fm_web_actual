eee840985f761398125e9399fca1271a615d98f8
jose <jose.s.contacto@gmail.com>
2024-12-17 18:14:12 -0300

    update

--- blog/js/scripts.js
+++ blog/js/scripts.js
@@ -30,3 +30,13 @@
   }
 });
 
+
+$( document ).ready(function () {
+    $("#anythingSearch").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#myDIV .card").filter(function () {
+            $(this).parent().toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+     });
+ });
+
